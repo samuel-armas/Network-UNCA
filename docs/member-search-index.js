c872a5103b434873cce42c27c25c2d9911b00e65
cano23 <eziocano23@hotmail.com>
2025-12-05 11:12:45 -0600

    Agregado Glosario Conceptual

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.networkunca","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"com.networkunca","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
